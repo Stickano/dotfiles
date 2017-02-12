328e3151947cd5c201f53adb4b483c9417c06748
Henrik Jeppesen <Jeppesen@tutanota.com>
2017-02-12 15:51:50 +0100

    Added all(-la) param to ls

--- .config/i3/layouts/load-ws10.sh
+++ .config/i3/layouts/load-ws10.sh
@@ -3,5 +3,5 @@ i3-msg "workspace 10: Damage Control; append_layout ~/dotfiles/.config/i3/lay
 
 (urxvt -hold -name ws01 -e zsh -c 'dmesg -w' &)
 (urxvt -hold -name ws02 -e zsh -c 'journalctl -xef' &)
-(urxvt -hold -name ws03 -e zsh -c 'systemd-analyze; cd /var/log; ls; zsh' &)
+(urxvt -hold -name ws03 -e zsh -c 'cd /var/log; ls -ll --group-directories-first --time-style=+"%d.%m.%Y %H:%M" --color=auto -F; systemd-analyze; zsh' &)
 (urxvt -hold -name ws04 -e zsh -c 'htop' &)
