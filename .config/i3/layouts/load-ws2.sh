383ada121118eb30262418aefc46a6c9909be12d
Henrik Jeppesen <Jeppesen@tutanota.com>
2017-02-12 15:52:07 +0100

    Added alsi

--- .config/i3/layouts/load-ws2.sh
+++ .config/i3/layouts/load-ws2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 i3-msg "workspace 2: term; append_layout ~/dotfiles/.config/i3/layouts/workspace_2.json"
 
-(urxvt -hold -name ws21 -e zsh -c 'zsh' &)
+(urxvt -hold -name ws21 -e zsh -c 'alsi -l; zsh' &)
 (urxvt -hold -name ws23 -e zsh -c 'cd /home/stick/dotfiles; git status; zsh' &)
 (urxvt -hold -name ws22 -e zsh -c 'ranger; zsh' &)
