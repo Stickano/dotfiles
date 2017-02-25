e560a97cc32f82cb5a1bfce5c972935d02ade27a
Henrik Jeppesen <Jeppesen@tutanota.com>
2017-02-25 17:34:22 +0100

    Changed Bar and Rofi layout

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -4,5 +4,5 @@ TMPBG=/tmp/screen.png
 scrot /tmp/screen.png
 convert $TMPBG -scale 10% -scale 1000% $TMPBG
 convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
-xbacklight -set 30%
+xbacklight -set 10%
 i3lock -u -i $TMPBG
