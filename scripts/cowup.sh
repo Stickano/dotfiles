e900d27b90b25e9ac2b7e96026846e75cd5694bb
Henrik Jeppesen <Jeppesen@tutanota.com>
2017-10-12 22:34:16 +0200

    Corrected update scripts

--- scripts/cowup.sh
+++ scripts/cowup.sh
@@ -2,4 +2,7 @@
 # Install multiple updates
 
 cd ~/makepkg/packages && find . -name PKGBUILD -execdir makepkg -sirc \;
-cd ~ && printf "%s\n" "Update Completed."
+cd ~ && printf "%s\n" "Update Completed"
+
+yes | rm -rf ~/makepkg/packages/*
+printf "%s\n" "Cleaned Packages"
